90a01da1c9dc431784e814f0d834f01c3095c94f
waiyee <103561708+waiyeewwy@users.noreply.github.com>
2023-07-10 15:43:57 +1000

    icons fixed

--- pages/faq.js
+++ pages/faq.js
@@ -47,7 +47,7 @@ export default function Faq() {
                     <li><Link href="/faq">FAQs</Link></li>
 
                     <a href="https://www.instagram.com/creamcheeseclub" target="_blank">
-                        <FontAwesomeIcon icon={faInstagram} />
+                        <FontAwesomeIcon icon={faInstagram} className={styles.ig} />
                     </a>
 
                     
